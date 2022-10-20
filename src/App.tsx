1090762f189bac2e7f4a6f7bd17dc640f748f90e
caneregin <36435160+caneregin@users.noreply.github.com>
2022-10-20 23:29:25 +0300

    taslak oluşturuldu

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Wrapper } from "./components/styled/index"
+
+const App = () => {
+  return (
+    <div>
+      <Wrapper>
+        <h1>App</h1>
+      </Wrapper>
+    </div>
+  )
+}
+
+export default App
